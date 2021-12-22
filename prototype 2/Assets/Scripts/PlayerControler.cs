e883db345812678366c85424d5f4ca1bb761dadc
Winstonfg <winston.grant@protonmail.com>
2021-12-21 23:06:50 -0800

    player move!
    added player movement and bounderies!

--- prototype 2/Assets/Scripts/PlayerControler.cs
+++ prototype 2/Assets/Scripts/PlayerControler.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     public float HorInput;
+
+    public float speed = 10.0f;
+
+    public float xbound = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +19,18 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -xbound)
+		{
+            transform.position = new Vector3(-xbound, transform.position.y, transform.position.z);
+		}
+        if (transform.position.x > xbound)
+        {
+            transform.position = new Vector3(xbound, transform.position.y, transform.position.z);
+        }
+
+
         HorInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * Time.deltaTime * speed * HorInput);
     }
 }
