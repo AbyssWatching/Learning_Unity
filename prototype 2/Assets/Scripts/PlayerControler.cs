40e88fd6bb5161fb0460431013dd46d93241f4bb
Winston Grant <winstonf.grant@hotmail.com>
2021-12-21 20:51:15 -0800

    added basic things
    added a toon soom animals and food I think I implemented horizontal movement or at least part of it!

--- prototype 2/Assets/Scripts/PlayerControler.cs
+++ prototype 2/Assets/Scripts/PlayerControler.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
+    public float HorInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        HorInput = Input.GetAxis("Horizontal");
     }
 }
