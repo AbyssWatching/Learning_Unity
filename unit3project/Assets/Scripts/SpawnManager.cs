b53c8876124b4d88c56badd4ea8307bc4a5b29a9
Winstonfg <winston.grant@protonmail.com>
2022-01-08 19:20:14 -0800

    Revert "so much so little time"
    This reverts commit afce2afd3728515b057504497a6de2e2c32c9820.

--- unit3project/Assets/Scripts/SpawnManager.cs
+++ unit3project/Assets/Scripts/SpawnManager.cs
@@ -4,31 +4,16 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    //just a position I WONDER/WANDER why we need that
-    private Vector3 obstaclePos = new Vector3(25, 1.2f, 0);
-    
-    //this will be implemented later
-    private float Rando;
-    
-    //THE BOLDER! 
     public GameObject obstacle;
-
-    //Time to make THE BOLDER!
+    private Vector3 obstaclePos = new Vector3(25, 1.2f, 0);
+    private float Rando;
     public float startTime = 2;
-
-    //SCRIPT
     public PlayerMovement PlayerMovementScript;
-    
     // Start is called before the first frame update
     void Start()
     {
-        //this will be implemented later
         Rando = Random.Range(.5f, 2.5f);
-
-        //Repeatedly spawns... THE BOLDER
         InvokeRepeating("SpawnRocks", startTime, 2);
-        
-        //WHAT ARE MY LINES, also the script
         PlayerMovementScript = GameObject.Find("Player").GetComponent<PlayerMovement>();
     }
 
@@ -37,10 +22,8 @@ public class SpawnManager : MonoBehaviour
     {
        
     }
-    //spawn Rocks hmmm wander what this does
     void SpawnRocks() 
     {
-        //if it's not over throw... THE BOLDER
         if (PlayerMovementScript.gameOver == false)
         {
             Instantiate(obstacle, obstaclePos, obstacle.transform.rotation);
--- unit3project/Assets/Scripts/moveLeft.cs
+++ unit3project/Assets/Scripts/moveLeft.cs
@@ -4,33 +4,23 @@ using UnityEngine;
 
 public class moveLeft : MonoBehaviour
 {
-    //setting boundaries cause that's important
     public float leftBound = -5; 
-
-    //Can't move to fast or they will get away but move to slow and they will want to be friends;p
     public float speed = 10.0f;
-    
-    //GET ME THAT SCRIPT BOI
     public PlayerMovement playerMovementScripts;
-    
     // Start is called before the first frame update
     void Start()
     {
-        //I SAID THE SCRIPT
         playerMovementScripts = GameObject.Find("Player").GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
     void Update()
 	{
-        //FOR THIS REASON
 		if (playerMovementScripts.gameOver == false )
 		{
-            //If he is alive throw rock at him bwahahaha
             transform.Translate(Vector3.right * Time.deltaTime * speed);
 
         }
-        //but destroy the rocks if they miss
 		if (gameObject.CompareTag("obstacle") && leftBound > gameObject.transform.position.x)
 		{
             Destroy(gameObject);
